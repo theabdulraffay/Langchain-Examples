292b721cc209e3e446ea00448e03ced3f4d99bae
theabdulraffay <abdulraffay2494@gmail.com>
2025-06-03 11:02:07 +0500

    feat: pydantic

--- structure output/pydantic_demo.py
+++ structure output/pydantic_demo.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr, Field
 from typing import Optional
+
+
+#Pydantic is a data validation and settings management library for Python.
+# It uses Python type annotations to validate data and provides a way to define data models.
 
 class Student(BaseModel):
     name: str = 'Unknown' # Default value for name
     age: Optional[int] = None # Optional field for age
     email: EmailStr # Email field with validation
+    cgpa: float = Field(gt=0, le=10, default=5, description="Decimal value representing the cgpa of the student") # CGPA must be greater than 0 and less than or equal to 10
 
 new_student= {
     "name": "John Doe",
@@ -14,3 +19,6 @@ new_student= {
 
 student = Student(**new_student)
 print(student)
+
+student_dist = student.model_dump_json()
+print(type(student_dist))
