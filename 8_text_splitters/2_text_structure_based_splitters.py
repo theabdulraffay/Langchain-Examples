939d42888abd5b469250a0c668c23f6fc3f80673
theabdulraffay <abdulraffay2494@gmail.com>
2025-06-15 19:21:53 +0500

    feat: text structure splitters

---
+++ 8_text_splitters/2_text_structure_based_splitters.py
@@ -0,0 +1,23 @@
+# it check the structure of the text,
+# it recursively breaks text ffirst by paragraphs then sentence, then words and then character. until it reached the desired chunk size
+# it also saves the context of the text
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+text = """
+Space exploration has led to incredible scientific discoveries. From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what’s possible beyond our planet.
+
+These missions have not only expanded our knowledge of the universe but have also contributed to advancements in technology here on Earth. Satellite communications, GPS, and even certain medical imaging techniques trace their roots back to innovations driven by space programs.
+"""
+
+# Initialize the splitter
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+)
+
+# Perform the split
+chunks = splitter.split_text(text)
+
+print(len(chunks))
+print(chunks)
