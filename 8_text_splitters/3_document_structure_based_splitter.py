7a331e61fbf0333246b4e7bd1f006f0e5263b339
theabdulraffay <abdulraffay2494@gmail.com>
2025-06-15 19:57:02 +0500

    feat: structure splitter

---
+++ 8_text_splitters/3_document_structure_based_splitter.py
@@ -0,0 +1,83 @@
+# lets say its a piece of code, and if you want to split that we use this splitter
+# markdown is also processed using this splitters
+# Differnt seperators are used in these splitters, 
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
+text = """
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # Grade is a float (like 8.5 or 9.2)
+
+    def get_details(self):
+        return self.name"
+
+    def is_passing(self):
+        return self.grade >= 6.0
+
+
+# Example usage
+student1 = Student("Aarav", 20, 8.2)
+print(student1.get_details())
+
+if student1.is_passing():
+    print("The student is passing.")
+else:
+    print("The student is not passing.")
+
+"""
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON,
+    chunk_size= 300,
+    chunk_overlap = 0
+)
+
+docs = splitter.split_text(text)
+
+print(docs[0])
+
+
+# we can use different languages in this way
+
+text = """
+# Project Name: Smart Student Tracker
+
+A simple Python-based project to manage and track student data, including their grades, age, and academic status.
+
+
+## Features
+
+- Add new students with relevant info
+- View student details
+- Check if a student is passing
+- Easily extendable class-based design
+
+
+## 🛠 Tech Stack
+
+- Python 3.10+
+- No external dependencies
+
+
+## Getting Started
+
+1. Clone the repo  
+   ```bash
+   git clone https://github.com/your-username/student-tracker.git
+
+"""
+
+# Initialize the splitter
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.MARKDOWN,
+    chunk_size=200,
+    chunk_overlap=0,
+)
+
+# Perform the split
+chunks = splitter.split_text(text)
+
+print(len(chunks))
+print(chunks[0])
