2f5988a28e50d79ad80c3bb6338ab9c96f591228
theabdulraffay <abdulraffay2494@gmail.com>
2025-06-24 01:52:05 +0500

    feat: multi query retriever

--- 10_retrievers/4_multi_query_retriever.py
+++ 10_retrievers/4_multi_query_retriever.py
@@ -4,7 +4,68 @@
 
 
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
 from langchain_core.documents import Document
-from langchain_openai import ChatOpenAI
 from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+os.environ['HF_HOME'] = 'C:\\Agentic AI'
+
+load_dotenv()
+
+model = ChatGroq(
+    model="meta-llama/llama-4-maverick-17b-128e-instruct",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+
+# Relevant health & wellness documents
+all_docs = [
+    Document(page_content="Regular walking boosts heart health and can reduce symptoms of depression.", metadata={"source": "H1"}),
+    Document(page_content="Consuming leafy greens and fruits helps detox the body and improve longevity.", metadata={"source": "H2"}),
+    Document(page_content="Deep sleep is crucial for cellular repair and emotional regulation.", metadata={"source": "H3"}),
+    Document(page_content="Mindfulness and controlled breathing lower cortisol and improve mental clarity.", metadata={"source": "H4"}),
+    Document(page_content="Drinking sufficient water throughout the day helps maintain metabolism and energy.", metadata={"source": "H5"}),
+    Document(page_content="The solar energy system in modern homes helps balance electricity demand.", metadata={"source": "I1"}),
+    Document(page_content="Python balances readability with power, making it a popular system design language.", metadata={"source": "I2"}),
+    Document(page_content="Photosynthesis enables plants to produce energy by converting sunlight.", metadata={"source": "I3"}),
+    Document(page_content="The 2022 FIFA World Cup was held in Qatar and drew global energy and excitement.", metadata={"source": "I4"}),
+    Document(page_content="Black holes bend spacetime and store immense gravitational energy.", metadata={"source": "I5"}),
+]
+
+
+# Initialize OpenAI embeddings
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", )
+
+# Create FAISS vector store
+vectorstore = FAISS.from_documents(documents=all_docs, embedding=embedding_model)
+
+# Create retrievers
+similarity_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+multiquery_retriever = MultiQueryRetriever.from_llm(
+    retriever=vectorstore.as_retriever(search_kwargs={"k": 5}),
+    llm= model,
+)
+
+# Query
+query = "How to improve energy levels and maintain balance?"
+
+# Retrieve results
+similarity_results = similarity_retriever.invoke(query)
+multiquery_results= multiquery_retriever.invoke(query)
+
+for i, doc in enumerate(similarity_results):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
+
+print("*"*150)
+
+for i, doc in enumerate(multiquery_results):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
