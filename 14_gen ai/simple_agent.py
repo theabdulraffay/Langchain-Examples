30bdc6f17b5259d4b951f6e8603b210c83947260
theabdulraffay <abdulraffay2494@gmail.com>
2025-07-14 18:29:28 +0500

    feat: simple agent

--- 14_gen ai/simple_agent.py
+++ 14_gen ai/simple_agent.py
@@ -7,6 +7,7 @@ from ddgs import DDGS
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain import hub
 from langchain_community.utilities import SearchApiAPIWrapper
+import os
 
 
 
@@ -17,6 +18,20 @@ search_tool = DuckDuckGoSearchRun()
 searching_tool = DDGS()
 search = SearchApiAPIWrapper()
 print(type(search))
+
+@tool
+def get_weather_data(city: str) -> str:
+  """
+  This function fetches the current weather data for a given city
+  """
+
+  key = os.getenv('WEATHERSTACK_API')
+#   print(key)
+  url = f'https://api.weatherstack.com/current?access_key={key}&query={city}'
+
+  response = requests.get(url)
+
+  return response.json()
 
 tool = Tool(
     name="search",
@@ -49,17 +64,19 @@ react_prompt = hub.pull("hwchase17/react") #pull the standard react agent prompt
 agent = create_react_agent(
     llm=model,
     prompt=react_prompt,
-    tools=[tool]
+    tools=[tool, get_weather_data]
 )
 
 agent_executor = AgentExecutor(
     agent=agent,
-    tools=[tool],
+    tools=[tool, get_weather_data],
     verbose=True,
-    max_iterations=3,  # limit the number of iterations
-    return_intermediate_steps=True  # return intermediate steps for debugging
+    # max_iterations=3,  # limit the number of iterations
+    # return_intermediate_steps=True  # return intermediate steps for debugging
 
 )
 
-response = agent_executor.invoke({'input':'What is the latest news on AI?'})
-print(response)
+response = agent_executor.invoke({'input':'What is the capital of Pakistan? whats the weather there?'})
+print(response['output'])
+
+# print(get_weather_data.invoke('Karachi'))
